87303c8ae45d7165b9594f3c3af945d742a73211
FeatherFly <164650591+18309444251@users.noreply.github.com>
2026-09-18 22:33:50 +0400

    Add files via upload

---
+++ C_From_Zero/C_From_Zero/day003.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*
+读入输入 -- scanf
+需要注意的是，使用scanf时，一定要确保scanf中引号中的内容要与&符中的绝对一致。
+*/
+
+//int main() {
+	//单次输入
+	//char name[100] = "";
+	//printf("请输入你的姓名：\n");
+	//scanf("%s", &name);
+	//printf("你的姓名是：%s\n",name);
+
+	//int age;
+	//printf("请输入你的年龄：");
+	//scanf("%d",&age);
+	//printf("我的年龄是：%d",age);
+
+	//一次性输入
+//	char name[100];  // 这是一个字符数组，必须规定长度，且需要注意的是，字符数组本身就占用着一个字符"\n",所以注意预留。
+//	int age;
+//	char genter[10];
+//
+//	printf("请输入你的姓名，年龄以及性别（使用空格隔开）：\n");
+//	scanf("%s %d %s", name, &age, genter);									// 关于&：整数，浮点数以及char单个字符需要加&符号，其他类型则不必加。
+//	printf("我叫%s，性别%s，今年%d岁。", name, genter, age);
+//
+//}    
+
+
+
+/*
+标识符的四个原则：
+1、只能以数字字母下划线命名；
+2、数字不可以开头；
+3、不可以是保留字（关键字）；
+4、好记忆。
+
+*/
+
